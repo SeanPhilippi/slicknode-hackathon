22140ab86cd23218c6a172ba106f35892bb03e4f
Jake Waltrip <jake.waltrip@gmail.com>
2019-02-16 18:58:44 -0600

    initial add game to user collection working

--- client/src/components/GameItems.js
+++ client/src/components/GameItems.js
@@ -1,9 +1,23 @@
 import React from "react";
-//var Linkify = require("react-linkify");
+import { ButtonDropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
+import { Mutation } from "react-apollo";
+import gql from "graphql-tag";
+
+const ADD_GAME_TO_USER = gql`
+    mutation AddGameToCollection($input: Gamecatalog_updateGameInput!) {
+        Gamecatalog_updateGame(input: $input) {
+            node {
+                id
+                title
+            }
+        }
+    }
+`;
 
 class GameItems extends React.Component {
   state = {
-    isHidden: true
+    isHidden: true,
+    dropdownOpen: false
   };
 
   toggleHidden = () => {
@@ -11,6 +25,11 @@ class GameItems extends React.Component {
       isHidden: !this.state.isHidden
     });
   };
+
+  toggleBtn = () => {
+    this.setState({ dropdownOpen: !this.state.dropdownOpen });
+  };
+
   render() {
     const { id, title, releaseDate, description, imgUrl, company } = this.props.single.node;
     const { isHidden } = this.state;
@@ -18,44 +37,89 @@ class GameItems extends React.Component {
 
     const desc = <h4>{description}</h4>;
 
-    console.log(this.props.single);
-
-    let buttons;
-    if (isHome) {
-      buttons = (
-        <span>
-          <button onClick={this.toggleHidden} className="btn btn-secondary text-dark mb-1">
-            Game Details
-          </button>
-          <button className="btn btn-primary">
-            Add to Collection
-          </button>
-        </span>
-      );
-    } else {
-      buttons = (
-        <button onClick={this.toggleHidden} className="btn btn-secondary text-dark">
-          Game Details
-        </button>
-      );
-    }
+    // console.log(this.props.single);
 
     return (
-      <div className="card card-body mb-3">
-        <div className="row">
-          <div className="col-md-10">
-            <h1>
-              {this.props.index + 1}: {title}
-            </h1>
-            <h3>Release Date: {releaseDate} </h3>
-            {!isHidden && desc}
+      <Mutation mutation={ADD_GAME_TO_USER}>
+        {(addGameToUser, { data }) => (
+          <div className="card card-body mb-3">
+            <div className="row">
+              <div className="col-md-10">
+                <h1>
+                  {this.props.index + 1}: {title}
+                </h1>
+                <h3>Release Date: {releaseDate} </h3>
+                {!isHidden && desc}
+              </div>
+              <div className="col-md-2 text-center">
+                { isHome ?
+                  (
+                    <span>
+                      <button onClick={this.toggleHidden} className="btn btn-secondary text-dark mb-1">
+                        Game Details
+                      </button>
+                      <ButtonDropdown isOpen={this.state.dropdownOpen} toggle={this.toggleBtn}>
+                        <DropdownToggle color="primary" caret>
+                          Add to Collection
+                        </DropdownToggle>
+                        <DropdownMenu>
+                          <DropdownItem header>To User:</DropdownItem>
+                          <DropdownItem onClick={() => {
+                            addGameToUser({
+                              variables: {
+                                input: {
+                                  id: id,
+                                  user: 'VXNlcjox'
+                                }
+                              }
+                            });
+                          }}>Sean</DropdownItem>
+                          <DropdownItem onClick={() => {
+                            addGameToUser({
+                              variables: {
+                                input: {
+                                  id: id,
+                                  user: 'VXNlcjoy'
+                                }
+                              }
+                            });
+                          }}>Zeke</DropdownItem>
+                          <DropdownItem onClick={() => {
+                            addGameToUser({
+                              variables: {
+                                input: {
+                                  id: id,
+                                  user: 'VXNlcjoz'
+                                }
+                              }
+                            });
+                          }}>Jake</DropdownItem>
+                          <DropdownItem onClick={() => {
+                            addGameToUser({
+                              variables: {
+                                input: {
+                                  id: id,
+                                  user: 'VXNlcjoz'
+                                }
+                              }
+                            });
+                          }}>Will</DropdownItem>
+                        </DropdownMenu>
+                      </ButtonDropdown>
+                    </span>
+                  ) :
+                    (
+                      <button onClick={this.toggleHidden} className="btn btn-secondary text-dark">
+                        Game Details
+                      </button>
+                    )
+                }
+                <a href={imgUrl} target={"_blank"}><img src={imgUrl} width={'100px'} style={{ marginTop: 15 }} /></a>
+              </div>
+            </div>
           </div>
-          <div className="col-md-2 text-center">
-            {buttons}
-            <a href={imgUrl} target={"_blank"}><img src={imgUrl} width={'100px'} style={{ marginTop: 15 }} /></a>
-          </div>
-        </div>
-      </div>
+        )}
+      </Mutation>
     );
   }
 }
