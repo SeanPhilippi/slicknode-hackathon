49dcbb22b656de6196a5ba08776248e67e7b5747
Jake Waltrip <jake.waltrip@gmail.com>
2019-02-19 01:15:32 -0600

    added page redirect onAdd to collection

--- client/src/components/Games.js
+++ client/src/components/Games.js
@@ -2,14 +2,21 @@ import React, { Component, Fragment } from "react";
 import { withRouter, Redirect } from 'react-router-dom';
 import gql from "graphql-tag";
 import { Query } from "react-apollo";
+import { Alert } from "reactstrap";
 import GameItems from "./GameItems";
 import LoadingPage from './LoadingPage';
 
 
 export class Games extends Component {
+  state = { isAlertOpen: true };
+
+  closeAlert = () => {
+    this.setState({ isAlertOpen: false });
+  };
+
   render() {
     // get user id from end of URL
-    const { pathname } = this.props.location;
+    const { pathname, state } = this.props.location;
     let id = pathname.split('/');
     id = id[id.length-1];
     console.log(id);
@@ -17,12 +24,24 @@ export class Games extends Component {
     // redirect to homepage if URL is just /games
     if (id === 'games' || id === '') return <Redirect to="/" />;
 
+    // check if a game was added to the collection
+    // if so, display a green alert w/ game title added
+    let newGameAlert = null;
+    if (state && state.gameAdded) {
+      console.log(state);
+      newGameAlert = (
+        <Alert color="success" isOpen={this.state.isAlertOpen} toggle={this.closeAlert}>
+          You added <b>{state.game}</b> to your collection!
+        </Alert>
+      );
+    }
+
     const QUERY_USER_GAMES = gql`
       {
         user: getUserById(id:"${id}") {
           firstName
           lastName
-          gameList: Gamecatalog_games {
+          gameList: Gamecatalog_games(first: 100) {
             totalCount
             games: edges {
               node {
@@ -53,6 +72,7 @@ export class Games extends Component {
               <Fragment>
                 <div className="container mt-3">
                   <h1 className="display-4 text-center mb-4 my-3 ps-blue"> {data.user.firstName+' '+data.user.lastName}'s Games </h1>
+                  {newGameAlert}
                   {data.user.gameList.games.map((single, idx) => (
                     <GameItems key={idx} index={idx} single={single} />
                   ))}
