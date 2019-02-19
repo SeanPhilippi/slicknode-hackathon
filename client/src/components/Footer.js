637adbfdd15412cab96299f51cb6ef00a0bf9ec1
William Cruz <chiefxcruz@gmail.com>
2019-02-19 07:43:17 -0600

    Changed year

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,17 +1,54 @@
-import React from 'react';
+import React from "react";
 
 const Footer = () => {
-  const chief = <a href="https://github.com/William-Cruz" target="_blank" rel="noopener noreferrer" >William Cruz</a>;
-  const jake = <a href="https://github.com/jwaltrip" target="_blank" rel="noopener noreferrer" >Jake Waltrip</a>;
-  const sean = <a href="https://github.com/SeanPhilippi" target="_blank" rel="noopener noreferrer" >Sean Philipi</a>;
-  const zeke = <a href="https://github.com/Zgutier5" target="_blank" rel="noopener noreferrer" >Zeke Gutierez</a>;
+  const chief = (
+    <a
+      href="https://github.com/William-Cruz"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      {" "}
+      William Cruz{" "}
+    </a>
+  );
+  const jake = (
+    <a
+      href="https://github.com/jwaltrip"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      {" "}
+      Jake Waltrip{" "}
+    </a>
+  );
+  const sean = (
+    <a
+      href="https://github.com/SeanPhilippi"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      {" "}
+      Sean Philipi{" "}
+    </a>
+  );
+  const zeke = (
+    <a
+      href="https://github.com/Zgutier5"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      {" "}
+      Zeke Gutierez{" "}
+    </a>
+  );
 
   return (
     <div className="container mt-5 mb-4">
       <hr />
       <div className="text-muted text-center footer">
-        Created by {chief}, {jake}, {sean}, {zeke} <i>for the</i> ACA Slicknode Hackathon - Feb 2018
-      </div>
+        Created by {chief}, {jake}, {sean}, {zeke} <i>for the </i> ACA Slicknode
+        Hackathon - Feb 2019{" "}
+      </div>{" "}
     </div>
   );
 };
